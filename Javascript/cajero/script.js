d1c6dbd369f82d857da58e6d161c12840b41e23d
Manuel Duran <manuelmid99@gmail.com>
2022-11-21 00:07:32 +0100

    Working on the click function

--- Javascript/cajero/script.js
+++ Javascript/cajero/script.js
@@ -1,6 +1,7 @@
-let dinero = 110;
-let valorTotal;
-let papeles;
+let imagen = document.getElementById("imagen");
+let dinero = 0;
+let papeles = 0;
+let div = 0;
 let entregado = [];
 
 class Billete {
@@ -17,24 +18,39 @@ let caja = [];
     caja.push(new Billete( 20 , 2 ));
     caja.push(new Billete( 10 , 2 ));
 
-
     
-    for( let b of caja ){
-
-        if( dinero > 0 ){
-            let papeles;
-            let div =  Math.floor( dinero / b.valor );
-
-            if( div > b.cantidad ){
-                papeles = b.cantidad;
-            }else{
-                papeles = div;
-            }
-
-            entregado.push(new Billete( b.valor , papeles ));
-            dinero = dinero - ( b.valor * papeles )
-            console.log(dinero)
-            console.log(caja[0])
-        }
+    
+    imagen.addEventListener("click", retirar);
+    
+    function retirar(){
         
-    }
+        let valorUser = document.getElementById("cajeroInput");
+        dinero = parseInt(valorUser.value);
+        console.log(dinero);    
+
+        for( let b of caja ){
+
+            if( dinero > 0 ){
+
+                let papeles;
+                let div =  Math.floor( dinero / b.valor );
+
+                if( div > b.cantidad ){
+                    papeles = b.cantidad;
+                }else{
+                    papeles = div;
+                }
+
+                entregado.push(new Billete( b.valor , papeles ));
+                dinero = dinero - ( b.valor * papeles );
+
+                console.log("weas "+ dinero)
+                //console.log( b.valor )
+
+            } 
+            
+            
+        }
+    console.log(entregado)
+
+}
