4afb27b315e1f854cbb50d639e3ba6c0fc559697
Manuel Duran <manuelmid99@gmail.com>
2022-11-26 19:48:32 +0100

    Starting with somes Arduino examples

---
+++ arduino/arduinoLeds.cpp
@@ -0,0 +1,21 @@
+int amarillo = 13;
+int rojo = 11;
+int miliSegundos = 500;
+
+void setup(){
+
+    pinmode( amarillo, OUTPUT );
+    pinmode( rojo, OUTPUT );
+
+}
+
+void loop(){
+
+    digitalWrite( amarillo, HIGH );
+    digitalWrite( rojo, LOW )     ;
+    delay( milisegundos )         ;
+    digitalWrite( amarillo, LOW ) ;
+    digitalWrite( rojo, HIGH )    ;
+    delay( milisegundos * 2 )     ;
+
+}
