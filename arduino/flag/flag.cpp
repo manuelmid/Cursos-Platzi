789045d2f5442a47b17751d9a542107dc0bb4ada
Manuel Duran <manuelmid99@gmail.com>
2022-12-04 20:52:07 +0100

    Making light capturing function that move a motor with cpp

--- arduino/flag/flag.cpp
+++ arduino/flag/flag.cpp
@@ -1,8 +1,42 @@
-int bombillo = 13;
-int turno = 0;
+#include <Servo.h>
+//LINK : https://www.tinkercad.com/things/d7ZfeMB9k4p-tremendous-habbi-kup/editel
 
-void setup(){
+Servo servo;
+int led = 13;
+int pinLDR = 0;
+int valorLDR = 0;
 
-    pinMode( bombillo , OUTOUT );
-    //Salida led rojo
+bool servoAbajo = true;
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  pinMode(13, OUTPUT); 
+  
+  servo.attach(9);
+  servo.write(0);
+}
+
+void loop()
+{
+  valorLDR = analogRead(pinLDR);
+  digitalWrite(led, HIGH);
+  
+  Serial.println(valorLDR);
+  
+  if (valorLDR > 500) {
+    if (servoAbajo) {
+      servo.write(110);
+      servoAbajo = false;
+    } else {
+      servo.write(70);
+      servoAbajo = true;
+    }
+  	
+  } else {
+  	servo.write(0);
+  }
+  
+  delay(1000);
 }
