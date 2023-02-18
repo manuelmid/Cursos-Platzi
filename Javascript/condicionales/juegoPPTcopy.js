41af080f2de368fa709045533fd14cb8a7d07186
manuelmid <manuelmid99@gmail.com>
2023-02-18 10:53:17 +0100

    game exercise with switch

---
+++ Javascript/condicionales/juegoPPTcopy.js
@@ -0,0 +1,35 @@
+var elements = ['piedra','papel','tijera'];
+var user = document.getElementById('cars');
+var pc = document.getElementById('carsPc');
+
+function jugar(){
+
+    var userV = user.value;
+    var pcV = pc.value;
+
+    switch(true){
+
+        case userV == pcV:
+            console.log("Empate")
+            break;
+        case pcV == elements[0] && userV == elements[1]:
+            console.log("Tu ganas con " + elements[1])
+            break;
+        case pcV == elements[1] && userV == elements[0]:
+            console.log("La computadora gana con " + elements[1])
+            break;
+        case pcV == elements[2] && userV == elements[0]:
+            console.log("Tu ganas con " + elements[0])
+            break;
+        case pcV == elements[0] && userV == elements[2]:
+            console.log("La computadora gana con " + elements[0])
+            break;
+        case pcV == elements[2] && userV == elements[1]:
+            console.log("La computadora gana con " + elements[2])
+            break;
+    
+    }
+
+}
+
+jugar()
